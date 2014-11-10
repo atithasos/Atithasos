3876c21df6e3b945fb1c0b78f0edec095dcb14ae
atithasos <atithasos@gmail.com>
2014-11-10 14:57:44 +0200

    Update installation.php

--- public/install/installation.php
+++ public/install/installation.php
@@ -5,7 +5,7 @@ $root_path = substr(getcwd(), 0, -7);
 <html lang="fr">
     <head>
         <meta charset="utf-8">
-        <title>Installation - Seedbox Manager</title>        
+        <title>Installation - Atithasos Seedbox Manager</title>        
         <link href="./components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" media="screen">
         <link type="text/css" rel="stylesheet" href="./install/style.css">
         <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1">
@@ -17,29 +17,29 @@ $root_path = substr(getcwd(), 0, -7);
 
     <body>
         <div  class="container marg" style="margin-top:50px">
-            <h1 class="page-header dashboard"><i class="glyphicon glyphicon-wrench"></i> Guide d'installation</h1>
+            <h1 class="page-header dashboard"><i class="glyphicon glyphicon-wrench"></i> Installation Guide </h1>
             <section class="row">
                 <article class="col-md-12">
                     <div class="well well-sm">
-                        <h4 class="titre-head">Démarrage de l'application</h4>
+                        <h4 class="titre-head">Starting the application</h4>
                         <div class="trait"></div>
-                        <p>Indiquez le bon propriétaire des fichiers de l'application, copiez cette commande et l'exécuter en ROOT (super utilisateur).</p>
+                        <p>Enter the correct owner of the application files, copy this command and run ROOT (superuser).</p>
                         <code>chown -R <?php echo $user_name_php['name'].':'.$user_name_php['name'].' '.$root_path; ?>/</code>
-                        <p>Exécutez le script install.sh pour compiler le programme de reboot en ROOT.</p>
+                        <p>Run the install.sh script to compile the program to reboot in ROOT.</p>
                         <code>cd <?php echo $root_path; ?>/source-reboot-rtorrent/ </code>
                         <code>chmod +x install.sh &amp;&amp; ./install.sh</code>
                     </div>
                 </article>
                 <article class="col-md-12">
                     <div class="well well-sm">
-                        <h4 class="titre-head">Comment obtenir les droits administrateurs ?</h4>
+                        <h4 class="titre-head">How to get administrator rights. ?</h4>
                         <div class="trait"></div>
-                        <p>Pour obtenir les droits administrateurs il faut avant cela exécuter les commandes ci-dessus.</p>
-                        <p>Ensuite rafraîchir cette page avec F5 par exemple. Cela aura pour conséquence de générer vos fichiers de configuration.</p>
-                        <p>Puis ouvrez votre fichier de configuration avec un éditeur de texte.</p>
+                        <p>To gain administrator privileges before it must execute the commands above.</p>
+                        <p>Then F5 to refresh this page for example. This will result to generate your configuration files.</p>
+                        <p>Then open your configuration file with a text editor.</p>
                         <code>nano <?php echo $root_path; ?>/conf/users/<?php echo $userName; ?>/config.ini</code>
-                        <small><em>Je vous conseille de copier cette commande après rafraichissement normalement cette page ne s'affichera plus jamais.</em></small>
-                        <p>Pour terminer, il vous suffit de mettre owner = yes à la place de no et de quitter en enregistrant.</p>
+                        <small><em>I suggest you copy this command after refresh this page normally will be displayed again.</em></small>
+                        <p>Finally, you just need to owner = yes instead of no and quit editing.</p>
                     </div>
                 </article>
             </section>
